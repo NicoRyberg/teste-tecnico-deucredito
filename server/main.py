171c04342fc2625e6f34515256e6b5cc3d03afc6
Antonio Moreno <amrh07@gmail.com>
2021-06-23 15:30:18 -0300

    Mock data endpoints created

--- server/main.py
+++ server/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+import json
 
 # local imports
 from server import config
 
 
 app = FastAPI()
+
+# data
+with open("server/salesorders.json") as f:
+    salesorders = json.load(f)
+
+
+with open("server/invoices.json") as g:
+    invoices = json.load(g)
 
 
 @app.get('/')
@@ -14,3 +23,27 @@ async def main():
         'health_check': 'ok',
         'version': config.SERVER_VERSION
     }
+
+
+@app.get('/salesorders')
+async def get_salesorders():
+    filters = ['id','customerid','extended','discount','uom','subtransactions',
+    'transactionnumber','transactiondate','paymentterms',]
+    salesorders_filter = [
+        {_filter: element[_filter] for _filter in filters    
+    }
+    for element in salesorders
+    ]
+    return {"salesorders": salesorders_filter}
+
+
+@app.get('/invoices')
+async def get_salesorder():
+    filters = ['quantity','customerid','extended','discount','purchasingunits',
+    'invoicedate','itemname','category',]
+    invoices_filter = [
+        {_filter: element[_filter] for _filter in filters    
+    }
+    for element in invoices
+    ]
+    return {"invoices": invoices_filter}    
